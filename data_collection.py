287325be0146cf362321773d650d2c15526fe4e6
honeymanstark <102430895+honeymanstark@users.noreply.github.com>
2023-06-26 14:35:36 +0530

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -5,7 +5,7 @@
 import glassdoor_scraper as gs 
 import pandas as pd 
 
-path = "C:/Users/Ken/Documents/ds_salary_proj/chromedriver"
+path = "C:/Users/Manan/Documents/ds_salary_proj/chromedriver"
 
 df = gs.get_jobs('data scientist',1000, False, path, 15)
 
